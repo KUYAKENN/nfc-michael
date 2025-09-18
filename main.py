ed21e49b56ab405baa742480a109ea04004488ae
KUYAKENN <kennethjhonaycardo@gmail.com>
2025-09-18 15:35:12 +0800

    push

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 import uvicorn
 
 app = FastAPI()
 
 # Mount static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/favicon.ico")
+def favicon():
+    return FileResponse("static/favicon.ico")
 
 if __name__ == '__main__':
     uvicorn.run(
