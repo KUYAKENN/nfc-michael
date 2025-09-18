6ea7eda2484bf99ab17d0ce240a521481163fcc6
KUYAKENN <kennethjhonaycardo@gmail.com>
2025-09-18 14:58:19 +0800

    push

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import uvicorn
 
 if __name__ == '__main__':
     uvicorn.run(
-        'src.app_module:http_server',
+        'src.app_module:app',  # Correctly reference the FastAPI instance
         host="0.0.0.0",
         port=5003,  # Updated port to avoid conflicts
         reload=True
